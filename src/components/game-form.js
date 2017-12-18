0fa25c8377a47591b5435562189470b16d32e29e
Benilyn <benilyn.gonzales@gmail.com>
2017-12-18 10:37:02 -0800

    add placeholder for input

--- src/components/game-form.js
+++ src/components/game-form.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './game-form.css';
 
 export default class GameForm extends React.Component {
 	render() {
 		return (
 			<form className="guessForm" onSubmit={this.props.onSubmitGuess}>
-              <input type="number" name="userGuess" />
+              <input 
+              		type="number" 
+              		name="userGuess"
+              		placeholder="Enter your Guess" />
               <input type="submit" value="Guess" />
           </form>
 		);
